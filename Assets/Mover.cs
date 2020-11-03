256aa80218474adfd529bb69dc0e6d217d687766
avi130 <57074327+avi130@users.noreply.github.com>
2020-11-03 12:28:33 +0200

    Initial commit

---
+++ Assets/Mover.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [Tooltip("Movement speed in meters per sec")] [SerializeField] float speed ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed= 6f;
+    }
+  //  transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2.8f, 2.8f), transform.position.y, transform.position.z);
+ //Vector3 input = new Vector3(Mathf.Clamp((inputLeft + inputRight) * velocidade * Time.deltaTime, -2.8f, 2.8f), 0, 0);
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow)&&Input.GetKey(KeyCode.UpArrow)){
+            transform.position+= new Vector3(speed*Time.deltaTime,speed*Time.deltaTime,0);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -42f,22f),Mathf.Clamp(transform.position.y, -2.8f, 3.8f) ,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow)&&Input.GetKey(KeyCode.DownArrow)){
+            transform.position+= new Vector3(speed*Time.deltaTime,-speed*Time.deltaTime,0);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -42f,22f),Mathf.Clamp(transform.position.y, -2.8f, 2.8f) ,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow)&&Input.GetKey(KeyCode.DownArrow)){
+            transform.position+= new Vector3(-speed*Time.deltaTime,-speed*Time.deltaTime,0);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -42f,22f),Mathf.Clamp(transform.position.y, -2.8f, 2.8f) ,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow)&&Input.GetKey(KeyCode.UpArrow)){
+            transform.position+= new Vector3(-speed*Time.deltaTime,speed*Time.deltaTime,0);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -42f,22f),Mathf.Clamp(transform.position.y, -2.8f, 3.8f) ,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.UpArrow)){
+            transform.position+= new Vector3(0,speed*Time.deltaTime,0);
+            transform.position = new Vector3(transform.position.x,Mathf.Clamp(transform.position.y, -2.8f, 3.8f) ,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.DownArrow)){
+            transform.position+= new Vector3(0,-speed*Time.deltaTime,0);
+            transform.position = new Vector3(transform.position.x,Mathf.Clamp(transform.position.y, -2.8f, 2.8f) ,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.position+= new Vector3(-speed*Time.deltaTime,0,0);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -42.8f, 22f), transform.position.y,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow)){
+            transform.position+= new Vector3(speed*Time.deltaTime,0,0);
+             transform.position = new Vector3(Mathf.Clamp(transform.position.x, -42.8f, 22f), transform.position.y,transform.position.z);
+        }
+    }
+}
---
+++ Assets/Mover2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover2 : MonoBehaviour
+{
+    [Tooltip("Movement speed in meters per sec")] 
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed=6f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKey(KeyCode.D)&&Input.GetKey(KeyCode.W)){
+            transform.position+= new Vector3(speed*Time.deltaTime,speed*Time.deltaTime,0);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -42f,22f),Mathf.Clamp(transform.position.y, -24f, -17f) ,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.D)&&Input.GetKey(KeyCode.S)){
+            transform.position+= new Vector3(speed*Time.deltaTime,-speed*Time.deltaTime,0);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -42f,22f),Mathf.Clamp(transform.position.y, -24f, -17f) ,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.A)&&Input.GetKey(KeyCode.S)){
+            transform.position+= new Vector3(-speed*Time.deltaTime,-speed*Time.deltaTime,0);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -42f,22f),Mathf.Clamp(transform.position.y, -24f, -17f) ,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.A)&&Input.GetKey(KeyCode.W)){
+            transform.position+= new Vector3(-speed*Time.deltaTime,speed*Time.deltaTime,0);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -42f,22f),Mathf.Clamp(transform.position.y, -24f, -17f) ,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.W)){
+            transform.position+= new Vector3(0,speed*Time.deltaTime,0);
+            transform.position = new Vector3(transform.position.x,Mathf.Clamp(transform.position.y, -24f, -17f) ,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.S)){
+            transform.position+= new Vector3(0,-speed*Time.deltaTime,0);
+            transform.position = new Vector3(transform.position.x,Mathf.Clamp(transform.position.y, -24f, -17f) ,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.A)){
+            transform.position+= new Vector3(-speed*Time.deltaTime,0,0);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -42.8f, 22f), transform.position.y,transform.position.z);
+        }
+        else if(Input.GetKey(KeyCode.D)){
+            transform.position+= new Vector3(speed*Time.deltaTime,0,0);
+             transform.position = new Vector3(Mathf.Clamp(transform.position.x, -42.8f, 22f), transform.position.y,transform.position.z);
+        }
+    }
+}
